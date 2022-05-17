998a47ec42ad55a9c34ed6ddd07d8b8c6d2a1327
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-17 10:36:47 +0600

    added service api

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ async function run() {
         const servicesCollection = client.db("doctors_portal").collection("services")
         const bookingsCollection = client.db("doctors_portal").collection("bookings")
 
-        app.get('/service', async(req, res)=>{
+        app.get('/service', async(req, res) =>{
+            const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
-            res.send(services)
+            res.send(services);
         })
 
         /**
