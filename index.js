53bcbfd79f15d331ca8070d570fa2c4ceb5e3551
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-05-19 08:13:29 +0600

    added jsonwebtoken

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -88,6 +89,8 @@ async function run() {
               $set: user,
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+            res.send(result, token);
           })
         }      
     finally {
